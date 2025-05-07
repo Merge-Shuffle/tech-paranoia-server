e030a67b025d3fb6c5133ddd0191bbd19d2b71ad
Serhii Kohutko <serhiikohutko@gmail.com>
2025-05-08 00:55:04 +0300

    batman

---
+++ src/main/java/org/example/techparanoiaserver/TechParanoiaServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.techparanoiaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechParanoiaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechParanoiaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/techparanoiaserver/TechParanoiaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.techparanoiaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechParanoiaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
