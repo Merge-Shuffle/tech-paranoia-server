fc0269f0f2f3e8b9a4c3a470ba6e798645aeba23
Serhii Kohutko <serhiikohutko@gmail.com>
2025-05-08 13:16:19 +0300

    feat(TP-5): add get question details by id method

--- src/main/java/org/example/techparanoiaserver/service/Question/QuestionServiceImpl.java
+++ src/main/java/org/example/techparanoiaserver/service/Question/QuestionServiceImpl.java
@@ -1,8 +1,30 @@
 package org.example.techparanoiaserver.service.Question;
 
+import org.example.techparanoiaserver.entity.Question.Question;
+import org.example.techparanoiaserver.exception.NoQuestionMatchingIdFoundException;
+import org.example.techparanoiaserver.repository.Question.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class QuestionServiceImpl implements QuestionService{
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Override
+    public Question getQuestionById(UUID id) {
+
+        Optional<Question> questionOptional =
+                questionRepository.getQuestionById(id);
+
+        if (questionOptional.isEmpty()){
+            throw new NoQuestionMatchingIdFoundException(id);
+        }
+
+        return questionOptional.get();
+    }
 }
