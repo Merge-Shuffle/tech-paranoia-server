e9a7f5c0226524008643cff324dac4ba81c4de2b
Serhii Kohutko <serhiikohutko@gmail.com>
2025-05-08 13:17:18 +0300

    feat(TP-5): add and configure question controller

---
+++ src/main/java/org/example/techparanoiaserver/controller/QuestionController.java
@@ -0,0 +1,30 @@
+package org.example.techparanoiaserver.controller;
+
+import org.example.techparanoiaserver.entity.Question.Question;
+import org.example.techparanoiaserver.service.Question.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/questions")
+public class QuestionController {
+
+    private QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService){
+        this.questionService = questionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Question> getQuestionById(@RequestParam("id") UUID id){
+        return ResponseEntity.ok(questionService.getQuestionById(id));
+    }
+
+}
