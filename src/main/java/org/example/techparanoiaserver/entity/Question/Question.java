9aa0708dedb2a50e44bef8ebddf34a88fdfef0f4
Serhii Kohutko <serhiikohutko@gmail.com>
2025-05-17 01:41:31 +0300

    test(TP-10): add new integration tests for getAllQuestions method and createQuestion

--- src/main/java/org/example/techparanoiaserver/entity/Question/Question.java
+++ src/main/java/org/example/techparanoiaserver/entity/Question/Question.java
@@ -32,7 +32,7 @@ public class Question {
     @Column(nullable = false)
     private Category category;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(
             name = "question_tags",
             joinColumns = @JoinColumn(name = "question_id")
@@ -41,7 +41,7 @@ public class Question {
     private Set<String> tags;
 
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<AdditionalSource> additionalSources;
 
 }
--- src/test/java/org/example/techparanoiaserver/service/Question/integration/QuestionServiceIntegrationTest.java
+++ src/test/java/org/example/techparanoiaserver/service/Question/integration/QuestionServiceIntegrationTest.java
@@ -5,6 +5,8 @@ import org.example.techparanoiaserver.entity.Question.AdditionalSourceType;
 import org.example.techparanoiaserver.entity.Question.Category;
 import org.example.techparanoiaserver.entity.Question.Question;
 import org.example.techparanoiaserver.entity.Question.QuestionDifficulty;
+import org.example.techparanoiaserver.exception.NoQuestionMatchingIdFoundException;
+import org.example.techparanoiaserver.repository.Question.QuestionRepository;
 import org.example.techparanoiaserver.request.QuestionCreateRequest;
 import org.example.techparanoiaserver.service.Question.QuestionService;
 import org.junit.jupiter.api.Assertions;
@@ -14,9 +16,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -24,6 +30,9 @@ public class QuestionServiceIntegrationTest {
 
     @Autowired
     private QuestionService questionService;
+
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @Test
     void testCreateQuestion_whenNullValueProvided_throwException(){
@@ -40,8 +49,38 @@ public class QuestionServiceIntegrationTest {
         request.setAdditionalSources(List.of(additionalSourceDto));
 
         //Act & Assert
-
         Assertions.assertThrows(DataIntegrityViolationException.class, () -> questionService.createQuestion(request));
+    }
+
+    @Test
+    void testCreateQuestion_returnSavedQuestion(){
+        //Arrange
+        QuestionCreateRequest request = new QuestionCreateRequest();
+        request.setTags(Set.of("hibernate", "junit", "maven"));
+        request.setTitle("Test Title");
+        request.setContent("Test Content");
+        request.setDifficulty(QuestionDifficulty.MEDIUM);
+        request.setCategory(Category.JAVA);
+
+        AdditionalSourceDto additionalSourceDto = new AdditionalSourceDto();
+        additionalSourceDto.setSourceType(AdditionalSourceType.YOUTUBE);
+        additionalSourceDto.setUrl("test.url");
+        request.setAdditionalSources(List.of(additionalSourceDto));
+
+        //Act & Assert
+
+        Question savedQuestion =
+                questionService.createQuestion(request);
+
+        Question questionFromDb =
+                questionService.getQuestionById(savedQuestion.getId());
+
+        Assertions.assertNotNull(savedQuestion);
+        Assertions.assertNotNull(questionFromDb);
+        assertThat(savedQuestion)
+                .usingRecursiveComparison()
+                .ignoringCollectionOrderInFields("tags", "additionalSources")
+                .isEqualTo(questionFromDb);
     }
 
     @Test
@@ -72,5 +111,37 @@ public class QuestionServiceIntegrationTest {
 
         Assertions.assertNotNull(deletedQuestion);
         Assertions.assertEquals(deletedQuestion.getId(), createdQuestion.getId());
+        Assertions.assertThrows(NoQuestionMatchingIdFoundException.class, () -> questionService.getQuestionById(id));
+    }
+
+    @Test
+    void testGetAllQuestions_returnList(){
+        //Arrange
+        List<Question> questionList =
+                new ArrayList<>(
+                        Stream.generate(Question::new)
+                                .map(this::populateObject)
+                                .limit(10)
+                                .toList()
+                        );
+
+        questionRepository.saveAll(questionList);
+
+        //Action
+        List<Question> result = questionService.getAllQuestions();
+
+        //Assert
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(result.size(), questionList.size());
+
+    }
+
+    private Question populateObject(Question question){
+        question.setCategory(Category.JAVA);
+        question.setDifficulty(QuestionDifficulty.MEDIUM);
+        question.setTitle("test");
+        question.setContent("test");
+        return question;
     }
 }
